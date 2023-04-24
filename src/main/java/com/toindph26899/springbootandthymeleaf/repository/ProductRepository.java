6987832a7c125e8898f348b5c3317108ff064934
DESKTOP-A8MA4R6\os <toindph26899@fpt.edu.vn>
2023-04-24 20:59:43 +0700

    create product repository

---
+++ src/main/java/com/toindph26899/springbootandthymeleaf/repository/ProductRepository.java
@@ -0,0 +1,23 @@
+package com.toindph26899.springbootandthymeleaf.repository;
+
+import com.toindph26899.springbootandthymeleaf.entities.Product;
+import com.toindph26899.springbootandthymeleaf.response.ProductResponse;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    @Query("\n" +
+            "select new com.toindph26899.springbootandthymeleaf.response.ProductResponse(p.id, p.productCode, " +
+            "p.productName, ca.categoryName, co.companyName, s.statusName, p.sellPrice) " +
+            "from Product p \n" +
+            "join Status s on p.statusId = s.id\n" +
+            "join Company co on p .companyId = co.id\n" +
+            "join Category ca on p .categoryId = ca.id")
+    List<ProductResponse> findAllCustom();
+
+}
