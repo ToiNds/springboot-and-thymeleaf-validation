40bba78d2d063c7c313d760af030b8bf85355512
DESKTOP-A8MA4R6\os <toindph26899@fpt.edu.vn>
2023-04-24 12:40:06 +0700

    setup - project with(thymeleaf, validation, mysql, lombok, ...)

---
+++ src/main/java/com/toindph26899/springbootandthymeleaf/SpringbootandthymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.toindph26899.springbootandthymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootandthymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootandthymeleafApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/toindph26899/springbootandthymeleaf/SpringbootandthymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toindph26899.springbootandthymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootandthymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
