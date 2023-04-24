38df88030ace9d27d1ac0e23b87ced6223be2f7e
DESKTOP-A8MA4R6\os <toindph26899@fpt.edu.vn>
2023-04-24 12:55:25 +0700

    create entity Category + mapping entity <--> database

--- src/main/java/com/toindph26899/springbootandthymeleaf/entities/Category.java
+++ src/main/java/com/toindph26899/springbootandthymeleaf/entities/Category.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -12,6 +13,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
+
+import java.util.List;
 
 @Entity
 @Table(name = "category")
@@ -33,5 +36,8 @@ public class Category {
 
     @Column(name = "category_name")
     private String categoryName;
+
+    @OneToMany(mappedBy = "categoryId")
+    private List<Product> productList;
 
 }
