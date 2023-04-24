ad9bb29a99631a13550bed78fba0f00166fecb20
DESKTOP-A8MA4R6\os <toindph26899@fpt.edu.vn>
2023-04-24 12:54:03 +0700

    create entity Product + mapping entity <--> database

---
+++ src/main/java/com/toindph26899/springbootandthymeleaf/entities/Product.java
@@ -0,0 +1,69 @@
+package com.toindph26899.springbootandthymeleaf.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.annotations.Type;
+
+import java.util.List;
+
+@Entity
+@Table(name = "product")
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Builder
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "product_code")
+    private String productCode;
+
+    @Column(name = "product_name")
+    private String productName;
+
+    @Column(name = "quantity")
+    private Long quantity;
+
+    @Column(name = "color")
+    private String color;
+
+    @Column(name = "sell_price")
+    private Double sellPrice;
+
+    @Column(name = "origin-price")
+    private Double originPrice;
+
+    @Column(name = "description")
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "status_id")
+    private Status statusId;
+
+    @ManyToOne
+    @JoinColumn(name = "company_id")
+    private Company companyId;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category categoryId;
+
+}
